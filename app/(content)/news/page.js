cdf1a08ffff7b245e49ad9a9a30baa2fd56653c1
HanyMohamed <hsaad9231@gmail.com>
2024-05-24 22:51:58 +0300

    Add local Backend and  fetch Data in server

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,17 @@
-import React from "react";
-import { DUMMY_NEWS } from "@/dummy-news";
 import Newslist from "@/components/News-list";
-const Newspage = () => {
+const Newspage = async () => {
+  const response = await fetch("http://localhost:8080/news");
+
+  const news = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Faled to fetch news");
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <Newslist news={DUMMY_NEWS} />
+      <Newslist news={news} />
     </>
   );
 };
